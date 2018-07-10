54e8a7dad75d74322b9420021f198cb1cdb1bf87
JP Lynch <JPLynch35@gmail.com>
2018-07-10 14:01:03 -0600

    Adds functionality for student edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,13 +12,27 @@ class StudentsController < ApplicationController
   end
 
   def create
-    student = Student.create(student_params)
-    if student.save
-      redirect_to student_path(student)
+    @student = Student.create(student_params)
+    if @student.save
+      redirect_to student_path(@student)
     else
       render :new
     end
   end
+
+    def edit
+      @student = Student.find(params[:id])
+    end
+
+    def update
+      @student = Student.find(params[:id])
+      @student.update(student_params)
+      if @student.save
+        redirect_to student_path(@student)
+      else
+        render :edit
+      end
+    end
 
   private
   def student_params
