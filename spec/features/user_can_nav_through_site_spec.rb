43c2270c6b5913b53f6b070286cada0df018af6b
JP Lynch <JPLynch35@gmail.com>
2018-07-10 14:17:29 -0600

    Adds tests for nav bar

---
+++ spec/features/user_can_nav_through_site_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'a visitor' do
+  describe 'visiting any student route' do
+    it 'can use a nav bar to create a student from /students' do
+      student_1 = Student.create(name: 'Bobby')
+      student_2 = Student.create(name: 'Sally')
+
+      visit students_path
+
+      click_on 'Create a Student'
+
+      expect(page).to have_content('Create a Student!')
+    end
+    it 'can use a nav bar to student index from /students/new' do
+      student_1 = Student.create(name: 'Bobby')
+      student_2 = Student.create(name: 'Sally')
+
+      visit new_student_path
+
+      click_on 'All Students'
+
+      expect(page).to have_content(student_1.name)
+      expect(page).to have_content(student_2.name)
+    end
+  end
+end
