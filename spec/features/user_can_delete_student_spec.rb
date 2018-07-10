7f5b994baec80f3155840f537c1bee988376b071
JP Lynch <JPLynch35@gmail.com>
2018-07-10 14:06:42 -0600

    Adds test for student delete

---
+++ spec/features/user_can_delete_student_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'a visitor' do
+  describe 'visiting /students' do
+    it 'can delete a student' do
+      student_1 = Student.create(name: 'Bobby')
+      student_2 = Student.create(name: 'Sally')
+
+      visit students_path
+      within '.student_2' do
+        click_on 'Delete'
+      end
+
+      expect(page).to have_content(student_1.name)
+      expect(page).to_not have_content(student_2.name)
+    end
+  end
+end
