7904ded2a4e3db3c5b7c4fb698b72d1a68af1ddd
JP Lynch <JPLynch35@gmail.com>
2018-07-10 14:20:38 -0600

    Adds more nav tests to test nav functionality from all pages

--- spec/features/user_can_nav_through_site_spec.rb
+++ spec/features/user_can_nav_through_site_spec.rb
@@ -12,11 +12,32 @@ describe 'a visitor' do
 
       expect(page).to have_content('Create a New Student!')
     end
+    it 'can use a nav bar to create a student from /students/new' do
+      student_1 = Student.create(name: 'Bobby')
+      student_2 = Student.create(name: 'Sally')
+
+      visit new_student_path
+
+      click_on 'Create a Student'
+
+      expect(page).to have_content('Create a New Student!')
+    end
     it 'can use a nav bar to student index from /students/new' do
       student_1 = Student.create(name: 'Bobby')
       student_2 = Student.create(name: 'Sally')
 
       visit new_student_path
+
+      click_on 'All Students'
+
+      expect(page).to have_content(student_1.name)
+      expect(page).to have_content(student_2.name)
+    end
+    it 'can use a nav bar to student index from /students/:id' do
+      student_1 = Student.create(name: 'Bobby')
+      student_2 = Student.create(name: 'Sally')
+
+      visit student_path(student_1)
 
       click_on 'All Students'
 
