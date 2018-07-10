bae838dfcd7ca60ed0ea23d54dd3e0663f52147f
JP Lynch <JPLynch35@gmail.com>
2018-07-10 15:15:19 -0600

    Adds more specificity to courses list on student show page

--- spec/features/user_can_see_a_lsit_of_courses_for_a_student_spec.rb
+++ spec/features/user_can_see_a_lsit_of_courses_for_a_student_spec.rb
@@ -7,12 +7,14 @@ describe 'a visitor' do
       student_2 = Student.create(name: 'Sally')
       course_1 = student_1.courses.create(title: 'Math')
       course_2 = student_1.courses.create(title: 'Science')
+      course_3 = student_2.courses.create(title: 'History')
 
       visit student_path(student_1)
 
       expect(page).to have_content(student_1.name)
       expect(page).to have_content(course_1.title)
       expect(page).to have_content(course_2.title)
+      expect(page).to_not have_content(course_3.title)
     end
   end
 end
