18b8755d245f2427879595a6f5fbad3bceb14bad
JP Lynch <JPLynch35@gmail.com>
2018-07-10 13:50:18 -0600

    Adds tests and functionality to create a new student

--- spec/features/user_can_create_student_spec.rb
+++ spec/features/user_can_create_student_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 describe 'a visitor' do
   describe 'visiting /students/new' do
-    it 'can create a new student' do
+    it 'can see a page to create a new student' do
+      visit new_student_path
+
+      expect(page).to have_content('Create a New Student!')
+    end
+    it 'can fill in form and create a new student' do
       student_1 = Student.create(name: 'Bobby')
       student_2 = Student.create(name: 'Sally')
       
       visit new_student_path
 
-      expect(page).to have_content('Create a New Student!')
+      fill_in :student_name, with: 'Harold'
+      click_on 'Create Student'
+
+      expect(current_path).to eq(students_path)
+      expect(page).to have_content('Harold')
     end
   end
 end
